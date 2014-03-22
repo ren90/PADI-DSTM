20902a6c339717857edc75769a407a213c743877
Renato Filipe Vieira <renato.filipe.vieira@gmail.com>
2014-03-22 21:15:16 +0000

    first Transactional Server stub added

---
+++ TransactionalServer/TransactionalServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionalServer
+{
+    class ServerRunner
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
