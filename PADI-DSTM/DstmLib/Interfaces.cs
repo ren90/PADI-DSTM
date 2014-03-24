38d8bdf63d9ab019f335d73fde3302bf32a77ae5
diogoleitao <diogo.n.leitao@gmail.com>
2014-03-24 17:23:30 +0000

    File missing xD

---
+++ PADI-DSTM/DstmLib/Interfaces.cs
@@ -0,0 +1,14 @@
+﻿namespace DSTMLib
+{
+    public interface MasterInterface
+    {
+        PADInt CreatePADInt(int uid);
+        PADInt AccessPADInt(int uid);
+    }
+
+	public interface ServerInterface
+	{
+		int Read();
+		void Write(int value);
+	}
+}
